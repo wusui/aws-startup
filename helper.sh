59f0244779e7b0c63f251f3908937ed8e39cad58
Warren Usui <warrenusui@localhost.localdomain>
2019-06-23 19:21:50 -0700

    Update to work for 6/20

--- helper.sh
+++ helper.sh
@@ -8,19 +8,11 @@
 #
 # ../notes/pull-secret contains a pull-secret (goes in data/pull-secret).
 #
-# get_oc.py is a python 3 script that gets the latest version of the oc
-# files that got built.
-#
 source ../notes/info
 IPADDR=`echo ${1} | tr '.' '-'`
 REGION=${REGION:-us-east-2}
 PEMFILE=${PEMFILE:-wusui.pem}
-temp1=`mktemp`
-temp2=`mktemp`
-x=`python3 get_oc.py`
-echo "export OCLIENT='${x}'" > ${temp1}
-cat ../notes/info ${temp1} > ${temp2}
-scp -i ../notes/${PEMFILE} ${temp2} ec2-user@ec2-${IPADDR}.${REGION}.compute.amazonaws.com:info
+scp -i ../notes/${PEMFILE} ../notes/info ec2-user@ec2-${IPADDR}.${REGION}.compute.amazonaws.com:info
 scp -i ../notes/${PEMFILE} ../notes/pull-secret ec2-user@ec2-${IPADDR}.${REGION}.compute.amazonaws.com:.
 scp -i ../notes/${PEMFILE} ./install_oc_and_ocsci.sh ec2-user@ec2-${IPADDR}.${REGION}.compute.amazonaws.com:.
 rm -rf ${temp1} ${temp2}
--- install_oc_and_ocsci.sh
+++ install_oc_and_ocsci.sh
@@ -1,83 +1,68 @@
 #
 # install ocs-ci
 #
-sudo yum -y install git wget make
-# INSTALL python3.7
-# taken from https://tecadmin.net/install-python-3-7-on-centos/
-sudo yum -y install gcc openssl-devel bzip2-devel libffi-devel
-cd /usr/src
-sudo wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tgz
-sudo tar xzf Python-3.7.3.tgz
-cd Python-3.7.3
-sudo ./configure --enable-optimizations
-sudo make altinstall
-sudo rm /usr/src/Python-3.7.3.tgz
-python3.7 -V
-python3.7 -m venv ~/venv
-
-#preliminary stuff done
-cd ~
+echo 'Starting install'
+#
+# Prerequisites, Step 1
+#
+sudo yum -y install git wget python3
+#
+# Prerequisites, Step 2
+#
 source ./info
-REGION=${REGION:-us-east-2}
-# INSTALL OC -- gets latest oc from mirror.openshift.com
-source ~/venv/bin/activate
-# pip install requests
-#OCLIENT=`python get_oc.py`
-OCLIENT=${OCLIENT:-openshift-client-linux-4.1.0-rc.5.tar.gz}
-sudo wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/${OCLIENT}
-sudo gunzip ${OCLIENT}
-TARFILE=`echo ${OCLIENT} | sed 's/.gz$//'` 
-sudo tar xvf ${TARFILE}
-sudo mv oc /usr/bin/oc
-sudo mv kubectl /usr/bin/kubectl
-
-# Get ocs-ci (from my forked repo)
-cd ~
-git clone https://github.com/${MYNAME}/ocs-ci
-cd ocs-ci
-pip install --upgrade pip
-pip install -r requirements.txt
-
-# aws setup
-mkdir ~/ocs-ci/.aws
-mkdir ~/ocs-ci/data
-
-cat <<EOF > ~/.gitconfig
-[user]
-name = ${FULLNAME}
-email = ${MYNAME}@redhat.com
-EOF
-
-cat <<EOF > ~/dot-ocs-ci-yaml
-email:
-   address: ${MYNAME}@redhat.com
-EOF
-
-cat <<EOF > ~/aws_config
+mkdir ~/.aws
+cat <<EOF > ~/.aws/config
 [default]
 region=${REGION}
 output=json
 EOF
-
-cat <<EOF > ~/aws_credentials
+cat <<EOF > ~/.aws/credentials
 [default]
 aws_access_key_id=${AWS_ACCESS_KEY_ID}
 aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}
 EOF
-
-cp ~/dot-ocs-ci-yaml ~/.ocs-ci.yaml
-cp ~/aws_config ~/ocs-ci/.aws/config
-cp ~/aws_credentials ~/ocs-ci/.aws/credentials
-cp ~/pull-secret ~/ocs-ci/data/pull-secret
-chmod 0400 ~/ocs-ci/data/pull-secret
-rm ~/dot-ocs-ci-yaml
-rm ~/aws_config
-rm ~/aws_credentials
-rm ~/pull-secret
-
-sudo yum install postfix mailx -y
-x=`hostname -f`
-sudo sed -i "s/#myhostname = virtual.domain.tld/myhostname = ${x}/" /etc/postfix/main.cf
-sudo systemctl start postfix
-sudo systemctl enable postfix
-
+#
+# Prerequisites, Step 3
+#
+wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux-4.1.2.tar.gz
+gunzip openshift-client-linux-4.1.2.tar
+tar xvf openshift-client-linux-4.1.2.tar
+sudo mv oc /usr/local/bin/oc
+sudo mv kubectl /usr/local/bin/kubectl
+#
+# Installing, Step 1
+#
+git clone https://github.com/${MYNAME}/ocs-ci
+#
+# Installing, Step 2
+#
+cd ocs-ci
+#
+# Installing, Step 3
+#
+mkdir ~/venv
+python3 -m venv ~/venv
+source ~/venv/bin/activate
+#
+# Installing, Step 4
+#
+pip install --upgrade pip
+#
+# Installing, Step 5
+#
+pip install -r requirements.txt
+#
+# Initial Setup OCS-CI config
+#
+cp conf/ocsci/default_config.yaml conf/default_config.yaml
+#
+# Initial Setup Pull-secret
+#
+mkdir data
+cp ~/pull-secret data/pull-secret
+chmod 0700 data/pull-secret
+#
+# Install will be something like:
+#
+# run-ci -m deployment --cluster-conf conf/ocs_basic_install.yml --cluster-name wusui-test --cluster-path /home/ec2-user/ocs-ci/ --deploy --log-level=DEBUG
+#
